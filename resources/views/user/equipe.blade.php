5e8bbd6817a112ce34b9f3dab3abd20341d80872
João Lucas <joaocoutod@gmail.com>
2023-02-17 08:35:33 -0400

    edit

--- resources/views/user/equipe.blade.php
+++ resources/views/user/equipe.blade.php
@@ -22,7 +22,7 @@
     <h2>{{ $team->nome }}</h2>
 
     @if($exibir)
-        <div class="text-center py-4">
+        <div class="text-center">
             <a class="btn btn-warning  m-1" data-bs-toggle="modal" data-bs-target="#deletarTeam{{$team->id}}" href="#">Configuração de time </a>
             <a href="#" class="btn btn-outline-warning m-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
@@ -34,13 +34,16 @@
 
 
     <!-- SOBRE -->
-    <div class="container" id="custom-cards">
+    <div class="container p-4" id="custom-cards">
         <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4  justify-content-center">
         
             <div class="col">
                 <div class="card card-cover  overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image:  linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/img/tumblr/majorbr.jpeg'); background-position: center center; background-repeat:no-repeat; background-size:100% 100%;">
-                    <div class="d-flex flex-column  p-5 pb-5 text-white text-shadow-1 text-center">
-                        <h3 class="pt-3 mt-3 mb-3 display-6 fw-bold">Titulos</h3>
+                    <div class="d-flex flex-column  p-5 pb-5 text-white text-shadow-1 text-center align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trophy mt-3 " viewBox="0 0 16 16">
+                            <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z"/>
+                        </svg>
+                        <p class="pt-3 mb-3 fs-4 fw-bold">Titulos</p>
                         <p class=" display-6 fw-bold">{{$team->titulos}}</p>
                     </div>
                 </div>
@@ -49,8 +52,11 @@
 
         <div class="col">
             <div class="card card-cover overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)), url('/img/tumblr/team.png'); background-position: center center; background-repeat:no-repeat; background-size:100% 100%;">
-                <div class="d-flex flex-column  p-5 pb-5 text-shadow-1  text-center">
-                    <h3 class="pt-3 mt-3 mb-3 display-6  fw-bold ">Membros</h3>
+                <div class="d-flex flex-column  p-5 pb-5 text-white text-shadow-1 text-center align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-people mt-3" viewBox="0 0 16 16">
+                        <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
+                    </svg>
+                    <h3 class="pt-3  mb-3  fs-4 fw-bold ">Membros</h3>
                     <p class=" display-6 fw-bold">{{$team->qtd_membros}}</p>
                 </div>
             </div>
@@ -83,13 +89,13 @@
                         <tr>
                             <th scope="row">{{$membro->nick}}</th>
                             <td>{{$membro->funcao}}</td>
-                            <td class="p-2"><a href="{{$membro->link_steam}}" class="btn btn-primary" target="_blank">Steam</a></td>
-                            <td class="p-2"><a href="{{$membro->link_faceit}}" class="btn btn-warning" target="_blank">Faceit</a></td>
+                            <td class="p-2"><a href="{{$membro->link_steam}}" class="btn btn-primary" target="_blank"  style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">Steam</a></td>
+                            <td class="p-2"><a href="{{$membro->link_faceit}}" class="btn btn-warning" target="_blank"  style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">Faceit</a></td>
                             <td class="p-1 text-center">
 
                                 @if($exibir)
                                 <div class="text-center">
-                                    <a data-bs-toggle="modal" data-bs-target="#deletarMembro{{$membro->id}}" class="btn btn-danger m-1" href="#">deletar</a>
+                                    <a data-bs-toggle="modal" data-bs-target="#deletarMembro{{$membro->id}}" class="btn btn-danger m-1" href="#"  style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">deletar</a>
                                 </div>
                                 
                                 <!-- MODAL DELETAR Team -->
@@ -120,6 +126,8 @@
                     </tbody>
                 </table>
             </div><!-- div table -->
+
+            @endif
 
             @if($exibir)
             <div class="pb-5">
